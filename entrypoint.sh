e3b393da3257faa21ddc9e937a428d2db20fad91
sh <rahmatylin.sasha@yandex.ru>
2024-05-13 15:39:22 +0300

    add superuser

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,15 @@ python manage.py flush --no-input
 python manage.py makemigrations
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
+
+if [ "$DJANGO_SUPERUSER_USERNAME" ]
+then
+    python manage.py createsuperuser \
+        --noinput \
+        --username $DJANGO_SUPERUSER_USERNAME \
+        --email $DJANGO_SUPERUSER_EMAIL
+fi
+
+exec "$@"
+
 gunicorn config.wsgi:application --bind 0.0.0.0:8000
