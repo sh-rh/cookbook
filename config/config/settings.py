47ef980414fe38ffdfb1e5a32371bdebf6266a02
sh <rahmatylin.sasha@yandex.ru>
2024-05-14 13:13:38 +0300

    debug fix

--- config/config/settings.py
+++ config/config/settings.py
@@ -3,12 +3,9 @@ from pathlib import Path
 from dotenv import load_dotenv
 import dj_database_url
 
-from django.core.management.utils import get_random_secret_key
-
 load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
